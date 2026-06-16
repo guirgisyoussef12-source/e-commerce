9c6d8d8b16d7475dc38111651a3fa029f5212445
guirgis <guirgisyoussef12@gmail.com>
2026-06-16 15:31:25 -0700

    last version

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('sign_up/', views.sign_up, name='sign_up'),
     path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
